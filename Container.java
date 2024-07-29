0dbbef483aa75369bc68b7edeac8499ad960c42c
dhelms33 <derrickhelms10@gmail.com>
2024-07-29 14:28:58 -0600

    Added generics

---
+++ Container.java
@@ -0,0 +1,18 @@
+public class Box<T> {
+    private T item;
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public T getItem() {
+        return item;
+    }
+}
+
+public interface Container<T> {
+    void add(T item);
+
+    T get(int index);
+
+}
---
+++ Utility.java
@@ -0,0 +1,8 @@
+public class Utility {
+    public static <T> void printArray(T[] arr) {
+        for (T element : arr) {
+            System.out.println(element);
+        }
+    }
+
+}
